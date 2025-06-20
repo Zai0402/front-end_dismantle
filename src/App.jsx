a69b72757cb00c5eeca785f1c143dd88a63184a0
Zai <leozhy7@gmail.com>
2025-06-20 10:19:31 +0700

    k

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,32 @@
-import { useState, useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './login';
-import TechnicianDashboard from './teknisi/dashboard';
-import Devices from './teknisi/devices';
-import TechnicianTeams from './teknisi/technicianTeams';
-import AdminDashboard from './admin/dashboard';
-import OntData from './admin/ontData';
-import Teknisi from './admin/teknisi';
-
-
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./login";
+import TechnicianDashboard from "./teknisi/dashboard";
+import Devices from "./teknisi/devices";
+import TechnicianTeams from "./teknisi/technicianTeams";
+import AdminDashboard from "./admin/dashboard";
+import OntData from "./admin/ontData";
+import Teknisi from "./admin/teknisi";
 
 function App() {
-
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login/>}/>
-        <Route path='/login' element={<Login/>}/>
+      <BrowserRouter basename="/front-end_dismantle">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
 
-        <Route path='/teknisi/dashboard' element={<TechnicianDashboard/>}/>
-        <Route path='/teknisi/devices' element={<Devices/>}/>
-        <Route path='/teknisi/teams' element={<TechnicianTeams/>}/>
+          <Route path="/teknisi/dashboard" element={<TechnicianDashboard />} />
+          <Route path="/teknisi/devices" element={<Devices />} />
+          <Route path="/teknisi/teams" element={<TechnicianTeams />} />
 
-        <Route path='/admin/dashboard' element={<AdminDashboard/>}/>
-        <Route path='/admin/data-ont' element={<OntData/>}/>
-        <Route path='/admin/tim' element={<Teknisi/>}/>
-
-      </Routes>
-    </BrowserRouter>
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          <Route path="/admin/data-ont" element={<OntData />} />
+          <Route path="/admin/tim" element={<Teknisi />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
